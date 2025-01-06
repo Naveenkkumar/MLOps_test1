733f8983b88f54c94ea8d438244ced2461b59790
Naveen Kumar <naveenkk201113@gmail.com>
2025-01-06 21:26:32 +0530

    updated

--- MLOps_test1.py
+++ MLOps_test1.py
@@ -1,4 +1,33 @@
 import pandas as pd
 import streamlit as st
+import pickle
+#import sklearn
+from sklearn.ensemble import RandomForestClassifier
 
-st.title('')
+st.title('Titanic passengers survival prediction')
+
+st.write('This is a simple example of a Machine Learning model deployment using Streamlit.')
+
+
+
+user_input_pclass = st.selectbox('Pclass', [1, 2, 3])
+user_input_sex = st.selectbox('Sex', ['Male', 'Female'])
+user_input_age = st.number_input('Age', min_value=0, max_value=100, value=30)
+
+rfclf = pickle.load(open('rfclf.pkl', 'rb'))
+def predict():
+    data = [[user_input_pclass,user_input_sex, user_input_age, 1,0,0,1,0]]
+
+    result = rfclf.predict(data)
+
+    return result
+
+if st.button('Predict'):
+    result = predict()
+    if result == 1:
+        st.write('Survived')
+    else:
+        st.write('Not survived')
+
+else:
+    st.write('Please input values to predict survival')
